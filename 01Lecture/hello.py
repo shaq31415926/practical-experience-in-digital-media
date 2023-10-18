cfaeeea0431282dc48e92d99a80bbf09af67b73b
shaq31415926 <sarah.haq11@gmail.com>
2023-10-18 21:38:36 +0200

    added code for class

---
+++ 01Lecture/hello.py
@@ -0,0 +1,9 @@
+import intro_to_streamlit as st
+
+# this will create a title with a laughing emoji in the streamlit app
+st.title("Hello World :laughing:")
+# creates a horizontal line
+st.write("---")
+
+# for a complete list of emojis: https://raw.githubusercontent.com/MarcSkovMadsen/awesome-streamlit/master/gallery/emojis/emojis.py
+
