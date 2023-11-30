6ffe4cdb61f6d9afbfbecc1eabd6feb3a4bf2dfe
shaq31415926 <sarah.haq11@gmail.com>
2023-11-30 14:47:04 +0100

    my cat app code from class

---
+++ 04Lecture/cat_app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import requests
+
+# code to config the page
+st.set_page_config(
+    page_title="Cat App",
+    page_icon=":cat:"
+)
+
+st.header("WELCOME TO MY :cat: APP", divider='rainbow')
+
+
+def get_content():
+    """Access the API and get the image URL"""
+    contents = requests.get('https://cataas.com//cat')
+    # contents = requests.get('https://cataas.com//cat/gif')
+    return contents.content
+
+
+def place_cat_image():
+    """Place the cat image from the cataas.com API"""
+    cat_image = get_content()
+    st.image(cat_image, width=200)
+
+
+# add a button
+st.button("Click here",
+          help="Click to get a cat image",
+          on_click=place_cat_image)
