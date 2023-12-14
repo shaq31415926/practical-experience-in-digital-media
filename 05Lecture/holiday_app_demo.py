0ac5ae30aefe2cfcbaf39c2c9d50a41efe9df2af
shaq31415926 <sarah.haq11@gmail.com>
2023-12-14 14:24:37 +0100

    updated image file path

--- 05Lecture/holiday_app_demo.py
+++ 05Lecture/holiday_app_demo.py
@@ -10,6 +10,7 @@ st.set_page_config(
 st.header("GOOD LUCK AND HAPPY HOLIDAYS", divider="rainbow")
 
 # add an image
-st.image("images/snowman.jpeg",
+file_path = "https://github.com/shaq31415926/practical-experience-in-digital-media/blob/main/05Lecture/images/snowman.jpeg?raw=true"
+st.image(file_path,
          #width=200,
          caption="IT'S A SNOWMAN")
