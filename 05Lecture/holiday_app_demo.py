cc967c17d66ff60792fe7b051384e2a74f8eb285
shaq31415926 <sarah.haq11@gmail.com>
2023-12-14 14:49:20 +0100

    testing multiple images

--- 05Lecture/holiday_app_demo.py
+++ 05Lecture/holiday_app_demo.py
@@ -13,12 +13,8 @@ st.header("GOOD LUCK AND HAPPY HOLIDAYS", divider="rainbow")
 # add an image
 #file_path = "images/snowman.jpeg"
 file_path1 = "https://github.com/shaq31415926/practical-experience-in-digital-media/blob/main/05Lecture/images/snowman.jpeg?raw=true"
-file_path2="https://www.creativefabrica.com/wp-content/uploads/2020/10/26/Snowman-Christmas-Vector-Illustration-Graphics-6284110-1-1-580x431.jpg"
+#file_path2="https://www.creativefabrica.com/wp-content/uploads/2020/10/26/Snowman-Christmas-Vector-Illustration-Graphics-6284110-1-1-580x431.jpg"
 
 st.image(file_path1,
-         #width=200,
-         caption="IT'S A SNOWMAN")
-time.sleep(5)
-st.image(file_path2,
          #width=200,
          caption="IT'S A SNOWMAN")
