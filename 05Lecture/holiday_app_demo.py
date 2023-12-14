a88ca984dec414475e7517086fa6c31c6eedf487
shaq31415926 <sarah.haq11@gmail.com>
2023-12-14 14:34:51 +0100

    testing image from google

--- 05Lecture/holiday_app_demo.py
+++ 05Lecture/holiday_app_demo.py
@@ -10,7 +10,9 @@ st.set_page_config(
 st.header("GOOD LUCK AND HAPPY HOLIDAYS", divider="rainbow")
 
 # add an image
-file_path = "https://github.com/shaq31415926/practical-experience-in-digital-media/blob/main/05Lecture/images/snowman.jpeg?raw=true"
+#file_path = "images/snowman.jpeg"
+#file_path = "https://github.com/shaq31415926/practical-experience-in-digital-media/blob/main/05Lecture/images/snowman.jpeg?raw=true"
+file_path="https://www.creativefabrica.com/wp-content/uploads/2020/10/26/Snowman-Christmas-Vector-Illustration-Graphics-6284110-1-1-580x431.jpg"
 st.image(file_path,
          #width=200,
          caption="IT'S A SNOWMAN")
