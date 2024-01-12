20acbf9cc89efa5c0b25b77bf187e837f3fab631
shaq31415926 <sarah.haq11@gmail.com>
2024-01-12 15:55:52 +0100

    helper functions

--- 07Lecture/src/helpers.py
+++ 07Lecture/src/helpers.py
@@ -3,6 +3,9 @@ import streamlit as st
 import pandas as pd
 
 def connect_to_deta(db_name):
+    """This connect to deta and creates a data base to store our data"""
+
+    # fetches the secret key
     deta = Deta(st.secrets["data_key"])
 
     # create a database
