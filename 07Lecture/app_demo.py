a16791523c8e4ecf6bff58669412ed63c7ace2f4
shaq31415926 <sarah.haq11@gmail.com>
2024-01-12 17:33:15 +0100

    smoothed the register user flow

--- 07Lecture/app_demo.py
+++ 07Lecture/app_demo.py
@@ -9,15 +9,31 @@ st.set_page_config(
     layout="wide"
 )
 
-# create a placeholder variable
+# create a placeholder variable, so I can delete the form widget after using it
 placeholder = st.empty()
+# create a flag which will once is True, will let the user go the game
 credentials_check = False
+
+# create some session variables to keep track of if this is the first time the user is clicking on a button
+if 'login_count' not in st.session_state:
+    st.session_state.login_count = 0
+if 'register_count' not in st.session_state:
+    st.session_state.register_count = 0
+
+# this is the definiton that will add one to the counter every time the user clicks on it
+def increment_login_counter():
+    st.session_state.login_count += 1
+
+def increment_register_counter():
+    st.session_state.register_count += 1
+
 
 # create a deta db to store and check my data
 base_name = "snake-demo"
 db = connect_to_deta(base_name)
 # if you need to create the structure of the base - you can delete this row of data later
 # db.insert({"user_name": "", "password": ""})
+
 
 # create the log in form
 with placeholder.form("Login"):
@@ -29,37 +45,44 @@ with placeholder.form("Login"):
     login_button = st.form_submit_button("Login")
     register_button = st.form_submit_button("Register")
 
+    # fetch the user data to carry out validations
+    user_data = fetch_data(db)  # fetching all the data I have stored on my user
+    user_names = list(user_data.user_name)  # identifying the list of users
+
     if login_button:
-        # fetch the user data to carry out validations
-        user_data = fetch_data(db)  # fetching all the data I have stored on my user
-        user_names = list(user_data.user_name)  # identifying the list of users
+        increment_login_counter()  # keep track of the number of times the user comes on this button
+        # if it's the users first time dont print anything
+        if st.session_state.register_count > 1:
+            # if user name exists in user name - change credentials check flag to True
+            if user_name in user_names:
+                # this selects the password of the user that is entering information
+                registered_password = list(user_data[user_data.user_name == user_name].password)[0]
 
-        # if user name exists in user name - change credentials check flag to True
-        if user_name in user_names:
-            # this selects the password of the user that is entering information
-            registered_password = list(user_data[user_data.user_name == user_name].password)[0]
-
-            if password == registered_password:
-                credentials_check = True
+                if password == registered_password:
+                    credentials_check = True
+                else:
+                    st.error("The username/password is not correct")
             else:
-                st.error("The username/password is not correct")
-        else:
-            st.error("Please provide correct user name or click on register as new user")
+                st.error("Please provide correct user name or click on register as new user")
 
     if register_button:
-        if len(user_name) == 0 and len(password) == 0:
-            st.warning('Please enter username and password', icon="⚠️")
-        elif len(user_name) == 0:
-            st.warning('Please enter username', icon="⚠️")
-        elif len(password) == 0:
-            st.warning('Please enter password', icon="⚠️")
-        elif user_name in user_names:
-            st.warning('This user name already exists. Please create another user name or click on Login', icon="⚠️")
-        else:
-            # write the data to the database and update the credentials check flag
-            db.put({"user_name": user_name,
-                    "password": password})
-            credentials_check = True
+        increment_register_counter()  # keep track of the number of times the user comes on this button
+        # if it's the users first time dont print anything
+        if st.session_state.register_count > 1:
+            if len(user_name) == 0 and len(password) == 0:
+                st.warning('Please enter username and password', icon="⚠️")
+            elif len(user_name) == 0:
+                st.warning('Please enter username', icon="⚠️")
+            elif len(password) == 0:
+                st.warning('Please enter password', icon="⚠️")
+            elif user_name in user_names:
+                st.warning('This user name already exists. Please create another user name or click on Login',
+                           icon="⚠️")
+            else:
+                # write the data to the database and update the credentials check flag
+                db.put({"user_name": user_name,
+                        "password": password})
+                credentials_check = True
 
 # once this flag has been updated, then we can go to the game
 if credentials_check:
