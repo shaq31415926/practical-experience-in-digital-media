139cc8757d688eaf0180cad929309e8592a0c643
shaq31415926 <sarah.haq11@gmail.com>
2024-01-25 15:29:28 +0100

    fixed the code to work for multipages

--- 07Lecture/app_demo.py
+++ 07Lecture/app_demo.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from src.snake_game import snake_game
+from src.snake_game_multipage import snake_game
 from src.helpers import connect_to_deta, fetch_data
 
 # set the page config info
@@ -19,6 +19,9 @@ if 'login_count' not in st.session_state:
     st.session_state.login_count = 0
 if 'register_count' not in st.session_state:
     st.session_state.register_count = 0
+if 'snake_count' not in st.session_state:
+    st.session_state.snake_count = 0
+
 
 # this is the definiton that will add one to the counter every time the user clicks on it
 def increment_login_counter():
@@ -35,58 +38,68 @@ db = connect_to_deta(base_name)
 # db.insert({"user_name": "", "password": ""})
 
 
-# create the log in form
-with placeholder.form("Login"):
-    st.markdown(f'<p style="font-size: 20px; color:grey">Hello! Please enter your log in info.'
-                f'<br>If this is your first time on my app then please click on the Register Button.</p>',
-                unsafe_allow_html=True)
-    user_name = st.text_input("Username", placeholder="Please enter your user name").lower()
-    password = st.text_input("Password", placeholder="Please enter your password", type="password")
-    login_button = st.form_submit_button("Login")
-    register_button = st.form_submit_button("Register")
+# create the log in form - this only gets created in the snake game has not been activated
+if st.session_state.snake_count < 1:
+    with placeholder.form("Login"):
+        st.markdown(f'<p style="font-size: 20px; color:grey">Hello! Please enter your log in info.'
+                    f'<br>If this is your first time on my app then please click on the Register Button.</p>',
+                    unsafe_allow_html=True)
+        user_name = st.text_input("Username", placeholder="Please enter your user name").lower()
+        password = st.text_input("Password", placeholder="Please enter your password", type="password")
+        login_button = st.form_submit_button("Login")
+        register_button = st.form_submit_button("Register")
 
-    # fetch the user data to carry out validations
-    user_data = fetch_data(db)  # fetching all the data I have stored on my user
-    user_names = list(user_data.user_name)  # identifying the list of users
+        # fetch the user data to carry out validations
+        user_data = fetch_data(db)  # fetching all the data I have stored on my user
+        user_names = list(user_data.user_name)  # identifying the list of users
 
-    if login_button:
-        increment_login_counter()  # keep track of the number of times the user comes on this button
-        # if it's the users first time dont print anything
-        if st.session_state.login_count > 1:
-            # if user name exists in user name - change credentials check flag to True
-            if user_name in user_names:
-                # this selects the password of the user that is entering information
-                registered_password = list(user_data[user_data.user_name == user_name].password)[0]
+        if login_button:
+            increment_login_counter()  # keep track of the number of times the user comes on this button
+            # if it's the users first time dont print anything
+            if st.session_state.login_count > 1:
+                # if user name exists in user name - change credentials check flag to True
+                if user_name in user_names:
+                    # this selects the password of the user that is entering information
+                    registered_password = list(user_data[user_data.user_name == user_name].password)[0]
 
-                if password == registered_password:
-                    credentials_check = True
+                    if password == registered_password:
+                        credentials_check = True
+                    else:
+                        st.error("The username/password is not correct")
                 else:
-                    st.error("The username/password is not correct")
-            else:
-                st.error("Please provide correct user name or click on register as new user")
+                    st.error("Please provide correct user name or click on register as new user")
 
-    if register_button:
-        increment_register_counter()  # keep track of the number of times the user comes on this button
-        # if it's the users first time dont print anything
-        if st.session_state.register_count > 1:
-            if len(user_name) == 0 and len(password) == 0:
-                st.warning('Please enter username and password', icon="⚠️")
-            elif len(user_name) == 0:
-                st.warning('Please enter username', icon="⚠️")
-            elif len(password) == 0:
-                st.warning('Please enter password', icon="⚠️")
-            elif user_name in user_names:
-                st.warning('This user name already exists. Please create another user name or click on Login',
-                           icon="⚠️")
-            else:
-                # write the data to the database and update the credentials check flag
-                db.put({"user_name": user_name,
-                        "password": password})
-                credentials_check = True
+        if register_button:
+            increment_register_counter()  # keep track of the number of times the user comes on this button
+            # if it's the users first time dont print anything
+            if st.session_state.register_count > 1:
+                if len(user_name) == 0 and len(password) == 0:
+                    st.warning('Please enter username and password', icon="⚠️")
+                elif len(user_name) == 0:
+                    st.warning('Please enter username', icon="⚠️")
+                elif len(password) == 0:
+                    st.warning('Please enter password', icon="⚠️")
+                elif user_name in user_names:
+                    st.warning('This user name already exists. Please create another user name or click on Login',
+                               icon="⚠️")
+                else:
+                    # write the data to the database and update the credentials check flag
+                    db.put({"user_name": user_name,
+                            "password": password})
+                    credentials_check = True
 
-# once this flag has been updated, then we can go to the game
-if credentials_check:
-    # delete all the form widgets we created
-    placeholder.empty()
-    # call the definition that launches the snake game
-    snake_game()
+    # once this flag has been updated, then we can go to the game
+    if credentials_check:
+        # delete all the form widgets we created
+        placeholder.empty()
+        # update the snake count to 1
+        st.session_state.snake_count = 1
+
+# if the snake count is greater than 0, this means the user has successfully passed the login checks
+# place the widgets
+# call the snake game definition
+if st.session_state.snake_count > 0:
+    # creating a side menu bar
+    menu = ["Introduction", "The Game"]
+    choice = st.sidebar.selectbox("Menu", menu)
+    snake_game(choice)
---
+++ 07Lecture/src/snake_game_multipage.py
@@ -0,0 +1,45 @@
+import streamlit as st
+
+
+def increment_snake_counter():
+    st.session_state.snake_count += 1
+
+
+def snake_game(choice):
+    # this keeps track if the user has entered the snake game
+    increment_snake_counter()
+
+    if choice == "Introduction":
+        # create subheader for the sections
+        st.subheader('| Intro')
+
+        c1, c2 = st.columns(2)
+        with c1:
+            st.image("images/snake_cartoon.png", caption="SNAKE IMAGE")
+        with c2:
+            intro_text = """
+            Welcome to the first streamlit-based Snake Game! Explore the captivating universe
+            of Snake, where the pursuit of high scores and the avoidance of collisions 
+            create an engaging and memorable gaming experience. With intuitive controls and 
+            beautiful graphics, this version of Snake will keep you entertained for hours.
+            """
+
+            # if you want to adjust the size and colour of the font you have to use some html
+            # update the font size and colour of your choice
+            st.write(f'<p style="font-size: 14px; color:grey">{intro_text}</p>',
+                     unsafe_allow_html=True)
+
+            # read and place an audio file in your streamlit app
+            audio_file = open("audio/speech.mp3", "rb")
+            audio_bytes = audio_file.read()
+            st.audio(audio_bytes, format="audio/mpeg")
+
+        st.subheader('| Instructions 🕹️')
+        st.subheader('| Code')
+
+    # Tab 2 is my game page which consists of an moving image right now
+    if choice == "The Game":
+        c1, c2, c3 = st.columns(3)
+        with c2:
+            st.image("https://media.giphy.com/media/VHOF8pfPZOt9p018zw/giphy.gif", width=400)
+
